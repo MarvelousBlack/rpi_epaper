5caf30738073d3aa0279d3c4d152b9946e0aa690
Megumi_fox <i@megumifox.com>
2020-07-04 16:47:57 +0800

    add license

--- demo/clock/clock.py
+++ demo/clock/clock.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+# Copyright (C) 2020 megumifox <i@megumifox.com>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from pathlib import Path
 import logging
