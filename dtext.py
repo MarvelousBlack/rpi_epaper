5c268e3334565e8502b9bc51e24838966c5390ad
Megumi_fox <i@megumifox.com>
2020-07-04 16:44:01 +0800

    clean up code and add license

--- dtext.py
+++ dtext.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+
+""" dtext a tool to """
+
+# Copyright (C) 2020 megumifox <i@megumifox.com>
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 import sys
+from pathlib import Path
 import logging
 from PIL import Image,ImageDraw,ImageFont
 import textwrap
 import traceback
 import pygame
 from pygame import freetype
+
+_font_init=pygame.freetype.init()
+
+wqyfont_12pt = pygame.freetype.Font(str(Path.cwd().joinpath('fonts','wenquanyi_12pt.pcf')),16)
+wqyfont_11pt = pygame.freetype.Font(str(Path.cwd().joinpath('fonts','wenquanyi_11pt.pcf')),14)
+wqyfont_10pt = pygame.freetype.Font(str(Path.cwd().joinpath('fonts','wenquanyi_10pt.pcf')),13)
+wqyfont_9pt  = pygame.freetype.Font(str(Path.cwd().joinpath('fonts','wenquanyi_9pt.pcf')),12)
+
 
 _cjkranges = [
   {"from": ord(u"\u3300"), "to": ord(u"\u33ff")},         # compatibility ideographs
@@ -46,19 +75,15 @@ class textbox:
                  color,
                  bgcolor,
                  alignment="L",
-                 row_spaces=3,
-                 Limage=True):
-        self.box_size_x = box_size[0] 
-        self.box_size_y = box_size[1] 
+                 row_spaces=3):
+        self.box_size_x,self.box_size_y = box_size 
         self.font = font
         self.alignment = alignment
         self.text_out_of_box = ""
         self.row_spaces = row_spaces
-        self.color = color
-        self.bgcolor = bgcolor
+        self.color = (color,color,color)
+        self.bgcolor = (bgcolor,bgcolor,bgcolor)
         self.y_offset = 0
-        self.box = (0,0) 
-        self.Limage = Limage
 
     def _split_text(self, text):
         """_split(text)(text: string) -> [string]
@@ -104,12 +129,12 @@ class textbox:
             self.y_offset = 0
         lines = self._split_text(text)
         strFormat = 'RGBA'
-        image = Image.new('RGBA', (self.box_size_x,self.box_size_y), self.bgcolor)
+        image = Image.new(strFormat, (self.box_size_x,self.box_size_y), self.bgcolor)
         draw = ImageDraw.Draw(image)
         for line in lines:
             height = int(self.font.size)
             if self.y_offset + height > self.box_size_y:
-                self.text_out_of_box += line
+                self.text_out_of_box += line + "\n"
             else:
                 rtext = self.font.render(line,self.color,self.bgcolor)
                 raw_str = pygame.image.tostring(rtext[0], strFormat, False)
@@ -124,27 +149,10 @@ class textbox:
                 else:
                     raise Exception("Invalid alignment!", self.alignment)
                 self.y_offset += (height+self.row_spaces)
-        if self.Limage :
-            image8bit = image.convert("L")
-            return image8bit
-        else:
-            return image
+        return image.convert("L")
     
     def draw_remain_text(self):
         if len(self.text_out_of_box) != 0:
             return self.draw_text(self.text_out_of_box)
         else:
-            image = Image.new('RGBA', (0,0), self.bgcolor)
-            return image
-
-    def draw_box(self,text,box,image,offset=False):
-        textimage=self.draw_text(text,offset)
-        self.box = box
-        image.paste(textimage,self.box,textimage)
-
-    def draw_next_box(self,image):
-        textimage=self.draw_remain_text()
-        image.paste(textimage,self.box,textimage)
-
-
-
+            return None
